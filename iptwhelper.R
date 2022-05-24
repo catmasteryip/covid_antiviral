aa5c84bb318a2bbc525162e2368aa3e2a31c1670
catmasteryip <29121012+catmasteryip@users.noreply.github.com>
2022-05-24 18:30:03 +0800

    modularised iptw in iptwhelper.R

---
+++ iptwhelper.R
@@ -0,0 +1,65 @@
+library(WeightIt)
+library(dplyr)
+library(tableone)
+
+iptw_tableone = function(df, 
+                         treatment, 
+                         x_factors, 
+                         path_of_unweighted_tableone,
+                         path_of_weighted_tableone){
+  #' IPTW and table one generation (unweighted and weighted), save to 
+  #'
+  #' @param df Dataframe. A dataframe that contains the treatment and x_factors
+  #' @param treatment string. A string that specifies the treatment column
+  #' @param x_factors c(string). A vector of strings that specfiy the covariate columns passed to IPTW
+  #' @param path_of_unweighted_tableone string. 
+  # A string that specifies the path to save unweighted table one in .xlsx
+  #' @param path_of_weighted_tableone string. 
+  # A string that specifies the path to save weighted table one in .xlsx
+  #' @return Weightit object/output that contains the weights and data, and other parameters
+  
+  # weightit 
+  function_call<-paste0("weight <- weightit(",treatment," ~ ",paste(x_factors, collapse = "+"), 
+                        ", data = df, method = \"ps\", 
+                estimand = \"ATT\")"
+  )
+  eval(parse(text = function_call))
+  tab1_unweighted = CreateTableOne(data = select(df, c(treatment, x_factors)), 
+                                   strata = treatment, 
+                                   test = F)
+  # unweighted table one
+  tab1_unweighted_Mat <- print(tab1_unweighted, quote = FALSE, 
+                               noSpaces = TRUE, printToggle = FALSE, smd = T)
+  tab1_unweighted_Mat = as.data.frame(cbind(" " = rownames(tab1_unweighted),tab1_unweighted))
+  ## Save to a xlsx
+  writexl::write_xlsx(tab1_unweighted_Mat, path = path_of_unweighted_tableone)
+  
+  # IPTW-ed table one
+  tab1_weighted = svyCreateTableOne(data = clus, strata = treatment, 
+                                    test = F)
+  tab1_weighted_Mat <- print(tab1_weighted, quote = FALSE, 
+                             noSpaces = TRUE, printToggle = FALSE, smd = T)
+  tab1_weighted_Mat = as.data.frame(cbind(" " = rownames(tab1_weighted),tab1_weighted))
+  ## Save to a xlsx
+  writexl::write_xlsx(tab1_weighted_Mat, path = path_of_weighted_tableone)
+  
+  return(weight)
+}
+
+iptw_regression_tidy = function(df, 
+                                weight, 
+                                outcome, 
+                                treatment){
+  #' IPTW-weighted regression to generate the exponentiated coeff/odds ratio given treatment to outcome
+  #'
+  #' @param df Dataframe. A dataframe that is used for iptw
+  #' @param weight Weightit object/output. which contains the weights and data, and other parameters
+  #' @param outcome String. A string that specifies the outcome column in df
+  #' @param treatment String. A string that specifies the treatment column in df
+  #' @return A DataFrame that contains the exponentiated coeff/odds ratio given treatment to outcome
+  
+  clus <- svydesign(id =~ 1, weights = weight$weights, data = ip_propensity)
+  res <- svyglm(paste0(outcome, " ~ ", treatment), design = clus,family = binomial)
+  tidy_reg = tidy(res, exponentiate = T, conf.int = T, conf.level = .95)
+  return(tidy_reg %>% filter(term != "(Intercept)"))
+}
