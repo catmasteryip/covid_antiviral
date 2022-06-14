83b852bf14c8db5029a9a0b94861127f1ee9c9fc
catmasteryip <29121012+catmasteryip@users.noreply.github.com>
2022-06-14 12:07:07 +0800

    culture positive excluded from organ dysfunction definition

--- iptwhelper.R
+++ iptwhelper.R
@@ -4,6 +4,7 @@ library(tableone)
 library(survival)
 library(survey)
 library(broom)
+library(boot)
 
 iptw_tableone = function(df, 
                          treatment, 
@@ -95,17 +96,33 @@ iptw_hr_tidy = function(df,
   hr.df = c()
   
   for(x in x_factors){
-    
+    if(x == "overall"){
+      # group by nothing 
+      group_var = c()
+    }else{
+      # group by x, or segment
+      group_var = c(x)
+    }
     hr.df.small = df %>%
-      group_by(!!as.name(x)) %>%
+      group_by_at(group_var) %>%
       do(models = tidy(
         coxph(formula = iformula, 
               data = .,
               weights = weight), 
         exponentiate = T, conf.int = T, conf.level = .95))%>%
-      unnest(models) %>%
-      mutate(segment = paste0(x, !!!syms(x))) %>%
-      select(-x) %>%
+      unnest(models) 
+    
+    if(x == "overall"){
+      hr.df.small = hr.df.small %>% 
+        mutate(segment = "overall")
+      
+    }else{
+      hr.df.small = hr.df.small %>% 
+        mutate(segment = paste0(x, !!!syms(x))) %>%
+        select(-x) 
+    }
+    # browser()
+    hr.df.small = hr.df.small %>%
       relocate(segment, .before = term) %>%
       select(-term)
     hr.df = rbind(hr.df, hr.df.small)
@@ -113,3 +130,146 @@ iptw_hr_tidy = function(df,
   
   hr.df
 }
+
+
+iptw_hr_boot = function(df, 
+                        x_factors,
+                        event,
+                        daystillevent,
+                        treatment){
+  #' IPTW-weighted univariate cox regression to generate the exponentiated hazard ratio given treatment to outcome
+  #'
+  #' @param df Dataframe. A dataframe that is for iptw and has iptw weight in column called "weight"
+  #' @param x_factors c(String). A vector of strings for category segmentation
+  #' @param event String. String specifying the event column
+  #' @param daystillevent String. String specifying no. of days till event
+  #' @param treatment String. A string that specifies the treatment column in df
+  #' @return A DataFrame that contains the exponentiated hazard ratio given treatment to outcome, 
+  #' segmented by each level of each factor in x_factors 
+  
+  iformula <- as.formula(paste("Surv(as.numeric(",daystillevent,"),as.numeric(", event,")) ~ ", treatment, collapse = ''))
+  hr.df = c()
+  
+  for(x in x_factors){
+    if(x == "overall"){
+        group_var = c()
+      }else{
+        group_var = c(x)
+      }
+    hr.df.small = df %>%
+      group_by_at(group_var) %>%
+      nest() %>%
+      mutate(boot.obj = map(.x = data,
+                            .f = ~boot(data = .x,
+                                       statistic = coxph.tidy,
+                                       R = 200,
+                                       stype = "i",
+                                       # coxph.tidy arguments
+                                       formula = iformula,
+                                       parallel = "multicore",
+                                       ncpus = 15))
+      ) %>%
+      select(-data) %>%
+      mutate(booted.ci = map(.x = boot.obj,
+                             .f = ~booting.ci(.x))) %>%
+      ungroup() %>%
+      unnest_wider(booted.ci) %>%
+      ungroup()
+    if(x == "overall"){
+      hr.df.small = hr.df.small %>% 
+        mutate(segment = "overall")
+      
+    }else{
+      hr.df.small = hr.df.small %>% 
+        mutate(segment = paste0(x, !!!syms(x))) %>%
+        select(-x) 
+    }
+    hr.df.small = hr.df.small %>%
+      ungroup() %>%
+      rename("estimate" = !!names(.[2]),
+             "conf.low" = !!names(.[3]),
+             "conf.high" = !!names(.[4])) %>%
+      
+      select(-boot.obj) 
+    hr.df = rbind(hr.df, hr.df.small)
+  }
+  
+  hr.df
+}
+
+# boostrap ci helpers
+
+coxph.tidy = function(df, i, formula){
+  # browser()
+  coxph.obj = coxph(formula = formula, 
+        data = df[i,],
+        weights = weight)
+  
+  results = tidy(coxph.obj, exponentiate = T, conf.int = T, conf.level = .95)
+  results = c(results$estimate, results$conf.low, results$conf.high)
+  return(results)
+}
+booting.ci = function(boot.obj){
+  booted.ci = boot.ci(boot.obj, conf = .95, type = c("perc"))
+  estimate = booted.ci$t0
+  ci = booted.ci$percent[4:5]
+  # add col names to results for return
+  return(c(estimate,ci))
+}
+par.boot.coxph = function(nested.df, iformula){
+  return(nested.df %>%
+    # partition(cluster) %>%
+    mutate(boot.obj = map(.x = data,
+                          .f = ~boot(data = .x,
+                                     statistic = coxph.tidy,
+                                     R = 200,
+                                     stype = "i",
+                                     # coxph.tidy arguments
+                                     formula = iformula,
+                                     parallel = "multicore",
+                                     ncpus = 15))
+    ))
+}
+
+
+library(parallel)
+
+# find out max. number of cores
+detectCores()
+# assign cores 
+cl <- makeCluster(getOption("cl.cores", 2))
+# 
+
+iformula = as.formula("Surv(as.numeric(daystillevent), as.numeric(death28d)) ~ paxlovid")
+x = "Sex"
+ip.iptw.mortality.p.boot.hr = ip.iptw.mortality.p %>%
+  group_by(!!as.name(x)) %>%
+  nest() %>%
+  mutate(boot.obj = map(.x = data,
+                        .f = ~boot(data = .x,
+                                   statistic = coxph.tidy,
+                                   R = 200,
+                                   stype = "i",
+                                   # coxph.tidy arguments
+                                   formula = iformula,
+                                   parallel = "multicore",
+                                   ncpus = 15))
+         ) %>%
+  select(-data) 
+ip.iptw.mortality.p.boot.hr %>%
+  mutate(booted.ci = map(.x = boot.obj,
+                         .f = ~booting.ci(.x))) %>%
+  
+  unnest_wider(booted.ci) %>%
+  rename("estimate" = !!names(.[3]),
+         "conf.low" = !!names(.[4]),
+         "conf.high" = !!names(.[5])) %>%
+  mutate(segment = paste0(x, !!!syms(x))) %>%
+  ungroup() %>%
+  select(-x, -boot.obj) 
+  
+
+# %>%
+#   relocate(segment, .before = term) %>%
+#   select(-term)
+
